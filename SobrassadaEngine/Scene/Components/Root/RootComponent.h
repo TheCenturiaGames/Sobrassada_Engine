b2424392093b69e4d9a3753f3c30a41a87c564dc
LukasGanter <44579580+LukasGanter@users.noreply.github.com>
2025-03-05 18:11:55 +0100

    Lg gizmos global mode tho 36 (#39)
    Reworked the gizmo system to use matrices instead of custom Transforms.
    Fixed all issues which were present in the previous implementation. 
    Two known issues, specified in the task, will be addressed in a separate bug report.

--- SobrassadaEngine/Scene/Components/Root/RootComponent.h
+++ SobrassadaEngine/Scene/Components/Root/RootComponent.h
@@ -11,26 +11,37 @@ enum ComponentMobilitySettings
 class RootComponent : public Component
 {
   public:
-    RootComponent(UID uid, UID uidParent, const Transform& parentGlobalTransform);
+    RootComponent(UID uid, UID uidParent, const float4x4& parentGlobalTransform);
+
     RootComponent(const rapidjson::Value& initialState);
+
     ~RootComponent() override;
 
-    void Update() override;
-    void RenderEditorInspector() override;
-    void RenderEditorComponentTree(UID selectedComponentUID) override;
     void Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) const override;
-    AABB& TransformUpdated(const Transform& parentGlobalTransform) override;
+
+    AABB& TransformUpdated(const float4x4& parentGlobalTransform) override;
 
     virtual bool CreateComponent(ComponentType componentType);
-    void RenderComponentEditor();
-    void RenderGuizmo();
 
-    bool IsSelectedComponent(UID componentUID) const { return selectedUID == componentUID; }
-    int GetMobilitySettings() const { return mobilitySettings; }
+    void RenderComponentEditor();
+    void RenderEditorComponentTree(UID selectedComponentUID) override;
+    void RenderEditorInspector() override;
+
+    void Update() override;
 
     void SetSelectedComponent(UID componentUID);
+    bool IsSelectedComponent(UID componentUID) const { return selectedUID == componentUID; }
+
+    int GetMobilitySettings() const { return mobilitySettings; }
+
+    const float4x4& GetParentGlobalTransform() override;
+
+  private:
+    Component* GetSelectedComponent();
 
   private:
     UID selectedUID;
-    int mobilitySettings = DYNAMIC;
+    int mobilitySettings         = DYNAMIC;
+
+    Component* selectedComponent = nullptr;
 };
