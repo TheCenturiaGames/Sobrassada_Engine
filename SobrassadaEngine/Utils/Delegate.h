0485da1c8de4ed90cfab86c6f402aad4a0d630b9
IDarkgenesis <byidarkgenesis@gmail.com>
2025-03-14 18:06:33 +0100

    Added Delegate and Event dispatcher classes

---
+++ SobrassadaEngine/Utils/Delegate.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <functional>
+
+template <typename ReturnValue, typename... Arguments> class Delegate
+{
+  public:
+    Delegate() = default;
+
+    Delegate(std::function<ReturnValue(Arguments...)>& newCallback) { callback = newCallback; };
+
+    Delegate(std::function<ReturnValue(Arguments...)>&& newCallback) { callback = std::move(newCallback); }
+
+    ~Delegate() = default;
+
+    ReturnValue Call(Arguments... args) { callback(args...); }
+
+  private:
+    std::function<ReturnValue(Arguments...)> callback;
+};
---
+++ SobrassadaEngine/Utils/EventDispatcher.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Delegate.h"
+#include <map>
+
+template <typename ReturnValue, typename... Arguments> class EventDispatcher
+{
+  public:
+    EventDispatcher() = default;
+    ~EventDispatcher() { subscribedCallbacks.clear(); };
+
+    int SubscribeCallback(Delegate<ReturnValue, Arguments...>&& delegate)
+    {
+        subscribedCallbacks.insert({++lastID, std::move(delegate)});
+
+        return lastID;
+    };
+
+    void RemoveCallback(int id)
+    {
+        auto iterator = subscribedCallbacks.find(id);
+        if (iterator != subscribedCallbacks.end()) subscribedCallbacks.erase(iterator);
+    }
+
+    void Call(Arguments... args)
+    {
+        for (auto& callback : subscribedCallbacks)
+        {
+            callback.second.Call(args...);
+        }
+    };
+
+  private:
+    std::map<int, Delegate<ReturnValue, Arguments...>> subscribedCallbacks;
+    int lastID = -1;
+};
+
+template <> class EventDispatcher<void>
+{
+  public:
+    EventDispatcher() = default;
+    ~EventDispatcher() { subscribedCallbacks.clear(); };
+
+    int SubscribeCallback(Delegate<void>&& delegate)
+    {
+        subscribedCallbacks.insert({++lastID, std::move(delegate)});
+
+        return lastID;
+    };
+
+    void RemoveCallback(int id)
+    {
+        auto iterator = subscribedCallbacks.find(id);
+        if (iterator != subscribedCallbacks.end()) subscribedCallbacks.erase(iterator);
+    }
+
+    void Call()
+    {
+        for (auto callback : subscribedCallbacks)
+        {
+            callback.second.Call();
+        }
+    };
+
+  private:
+    std::map<int, Delegate<void>> subscribedCallbacks;
+    int lastID = -1;
+};
