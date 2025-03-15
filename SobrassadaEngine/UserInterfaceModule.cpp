e950de224b432e210c56a864f2328f74ca6ddd1a
Lukas <lukasganter@gmail.com>
2025-03-15 02:27:55 +0100

    Fixed merge errors

--- SobrassadaEngine/UserInterfaceModule.cpp
+++ SobrassadaEngine/UserInterfaceModule.cpp
@@ -1,24 +1,30 @@
 #include "UserInterfaceModule.h"
 
+#include "Application.h"
+#include "ProjectModule.h"
+
 //Test to see if library is working
 bool UserInterfaceModule::Init()
 {
-    FT_Library library;
-    FT_Face face;
-
-    if (FT_Init_FreeType(&library))
+    if (App->GetProjectModule()->IsProjectLoaded())
     {
-        GLOG("Error: Could not initialize FreeType");
+        FT_Library library;
+        FT_Face face;
+
+        if (FT_Init_FreeType(&library))
+        {
+            GLOG("Error: Could not initialize FreeType");
+        }
+
+        if (FT_New_Face(library, "./EngineDefaults/Shader/Font/arial.ttf", 0, &face))
+        {
+            GLOG("Error: Could not load the font");
+        }
+
+        GLOG("Font loaded successfully: %s %s", face->family_name, face->style_name);
+
+        FT_Done_Face(face);
+        FT_Done_FreeType(library);   
     }
-
-    if (FT_New_Face(library, "./test/arial.ttf", 0, &face))
-    {
-        GLOG("Error: Could not load the font");
-    }
-
-    GLOG("Font loaded successfully: %s %s", face->family_name, face->style_name);
-
-    FT_Done_Face(face);
-    FT_Done_FreeType(library);
     return true;
 }
