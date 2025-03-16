1a18316c327954d46e59344ae477c00362f42c6c
Gold <dzharzard96@gmail.com>
2025-03-16 19:14:21 +0100

    Added License and about
    Added License for ImNodeFlow and About in the editor.

--- SobrassadaEngine/Modules/EditorUIModule.cpp
+++ SobrassadaEngine/Modules/EditorUIModule.cpp
@@ -922,6 +922,7 @@ void EditorUIModule::About(bool& aboutMenu) const
     ImGui::Text(" - JSON: rapidjson v1.1");
     ImGui::Text(" - UI: FreeType: v2.13.3");
     ImGui::Text(" - RecastNavigation: v1.6.0");
+    ImGui::Text(" - ImNodeFlow: v1.2.2");
     ImGui::Text("%s is licensed under the MIT License, see LICENSE for more information.", ENGINE_NAME);
 
     static bool show_config_info = false;
