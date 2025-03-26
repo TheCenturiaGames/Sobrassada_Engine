0cd3f7f49ff7e943690bba7bdd1764e9feb2b9b8
Marc López <88272783+MarcLopezS@users.noreply.github.com>
2025-03-26 16:39:33 +0100

    Update keys mov character controller THO-324 (#79)
    * Added movement on X axis
    
    - Has been added movement for the character controller to move on X axis.
    - Keys A & D are binded to move left or right respectively.
    - Rotation now is binded to Q & E keys.
    
    * updated keyboard input in Character Controller
    
    - Target direction itself was doing nothing in this part and was restricting the diagonal movements when pressing two keys (ex: A and W). Now it is solved.
    
    - Target direction will be used for rotating the gameObject itself when changing directions for smooth movements (future update).
    
    NOTE: Has been found a bug when play button is pressed and afterwards  stop button is pressed more than once, it unables to execute the engine. To solve this, you are forced to delete the save scene that was using.

--- SobrassadaEngine/Scene/Components/Standalone/CharacterControllerComponent.cpp
+++ SobrassadaEngine/Scene/Components/Standalone/CharacterControllerComponent.cpp
@@ -196,17 +196,22 @@ void CharacterControllerComponent::HandleInput(float deltaTime)
 
     float3 direction(0.0f, 0.0f, 0.0f);
 
-    if (keyboard[SDL_SCANCODE_W] == KEY_REPEAT) direction -= targetDirection;
-    if (keyboard[SDL_SCANCODE_S] == KEY_REPEAT) direction += targetDirection;
+    if (keyboard[SDL_SCANCODE_W] == KEY_REPEAT) direction.z -= 1.0f;
+    if (keyboard[SDL_SCANCODE_S] == KEY_REPEAT) direction.z += 1.0f;
+    if (keyboard[SDL_SCANCODE_A] == KEY_REPEAT) direction.x -= 1.0f;
+    if (keyboard[SDL_SCANCODE_D] == KEY_REPEAT) direction.x += 1.0f;
 
     float rotationDir = 0.0f;
 
-    if (keyboard[SDL_SCANCODE_A] == KEY_REPEAT) rotationDir += 1.0f;
-    if (keyboard[SDL_SCANCODE_D] == KEY_REPEAT) rotationDir -= 1.0f;
+    if (keyboard[SDL_SCANCODE_Q] == KEY_REPEAT) rotationDir += 1.0f;
+    if (keyboard[SDL_SCANCODE_E] == KEY_REPEAT) rotationDir -= 1.0f;
 
     if (direction.LengthSq() > 0.0001f)
     {
         direction.Normalize();
+        targetDirection = direction;
+
+        // TODO: Handle rotation of gameObject when changing directions (target direction will be used in this part)
         Move(direction, deltaTime);
     }
 
