6221e1c74e3ee4c8ede138c0f77b058f72ac5a78
Joan Llobet <j.llobetruiz@gmail.com>
2025-04-01 23:32:43 +0200

    small fix

--- SobrassadaEngine/ResourceManagement/Resources/ResourceNavmesh.cpp
+++ SobrassadaEngine/ResourceManagement/Resources/ResourceNavmesh.cpp
@@ -351,11 +351,12 @@ bool ResourceNavMesh::BuildNavMesh(
             }
         }
 
-        CreateDetourData();
-
-        delete context;
-        return true;
     }
+
+    CreateDetourData();
+
+    delete context;
+    return true;
 }
 
 void ResourceNavMesh::CreateDetourData()
