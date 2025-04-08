6645bd2cc2ba32afb0e3c978d647e299c2779f77
Bartomeu Perelló <44304824+IDarkgenesis@users.noreply.github.com>
2025-04-08 13:31:34 +0200

    THO-238 Physics (#84)
    * Added barebone physics module
    
    * Added temporal objects simulation
    
    * Added debug render to physics world
    
    * Set update of physics world when on play mode and cleaning when reseting scene
    
    * Forward declare in Component.h and Base Cube collider component
    
    * Initial bullet motion state
    
    * Created BulletMotionState for synchronization of transforms between worlds
    
    * Added rigid body to collider component and removed from dynamic world
    
    * Cube collider component saved and reload in scene
    
    * Added functionality to notify components when parent has had an update, colliders update position when parent stops dragging
    
    * Updating transform from physics world working
    
    * Fixed bullet motion state center offset and added ui edit properties
    
    * Added group and collision masks to dynamic world, manual change pending
    
    * Cube collider adapts to AABB of parent or children object
    
    * Added size to editor
    
    * Setting dynamic collider sets the mobility of the game object heriarchy
    
    * Added Collider type flags
    
    * Checking user pointers for collisions
    
    * Collision event callback working properly and fix on object picking
    
    * Added physics config in editor settings
    
    * Layer configuration being saved in scene file
    
    * Added sphere collider component and toggle for rendering physics world
    
    * Added capsule collider
    
    * Components adapt to scale changes in container GameObject
    
    * Added is top parent in game object to stop iterating in object picking
    
    * Added generate callback config on collider components
    
    * Ignore script dll
    
    * Added bullet include in release settings
    
    * Fixed typo
    
    * Added fit to size checkbox and fixed collider not adapting to single mesh
    
    * Check for AABB to be finite and not degenerate for updaating colliders
    
    * Change to ensure static objects have 0 mass
    
    * PR comments
    
    * Fixing merge errors
    
    * Updaing parent mobility properly on component creation
    
    * Updating mobility in hierarchy affects collider type
    
    * Delete lib from git

---
+++ SobrassadaEngine/Scene/Components/Standalone/Physics/CapsuleColliderComponent.cpp
@@ -0,0 +1,210 @@
+#include "CapsuleColliderComponent.h"
+
+#include "Application.h"
+#include "GameObject.h"
+#include "PhysicsModule.h"
+
+#include "ImGui.h"
+#include "Libs/rapidjson/document.h"
+
+CapsuleColliderComponent::CapsuleColliderComponent(UID uid, GameObject* parent)
+    : Component(uid, parent, "Capsule Collider", COMPONENT_CAPSULE_COLLIDER)
+{
+
+    parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+
+    CalculateCollider();
+
+    onCollissionCallback = CollisionDelegate(
+        std::bind(&CapsuleColliderComponent::OnCollision, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
+    userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    App->GetPhysicsModule()->CreateCapsuleRigidBody(this);
+}
+
+CapsuleColliderComponent::CapsuleColliderComponent(const rapidjson::Value& initialState, GameObject* parent)
+    : Component(uid, parent, "Capsule Collider", COMPONENT_CAPSULE_COLLIDER)
+{
+    if (initialState.HasMember("FreezeRotation")) freezeRotation = initialState["FreezeRotation"].GetBool();
+    if (initialState.HasMember("Mass")) mass = initialState["Mass"].GetFloat();
+    if (initialState.HasMember("ColliderType")) colliderType = ColliderType(initialState["ColliderType"].GetInt());
+    if (initialState.HasMember("ColliderLayer")) layer = ColliderLayer(initialState["ColliderLayer"].GetInt());
+    if (initialState.HasMember("GenerateCallback")) generateCallback = initialState["GenerateCallback"].GetBool();
+    if (initialState.HasMember("Radius")) radius = initialState["Radius"].GetFloat();
+    if (initialState.HasMember("Length")) length = initialState["Length"].GetFloat();
+    if (initialState.HasMember("FitToSize")) fitToSize = initialState["FitToSize"].GetBool();
+
+    if (initialState.HasMember("CenterOffset"))
+    {
+        const rapidjson::Value& dataArray = initialState["CenterOffset"];
+        centerOffset                      = {dataArray[0].GetFloat(), dataArray[1].GetFloat(), dataArray[2].GetFloat()};
+    }
+
+    if (initialState.HasMember("CenterRotation"))
+    {
+        const rapidjson::Value& dataArray = initialState["CenterRotation"];
+        centerRotation                    = {dataArray[0].GetFloat(), dataArray[1].GetFloat(), dataArray[2].GetFloat()};
+    }
+
+    onCollissionCallback = CollisionDelegate(
+        std::bind(&CapsuleColliderComponent::OnCollision, this, std::placeholders::_1, std::placeholders::_2)
+    );
+
+    if (colliderType == ColliderType::STATIC && !parent->IsStatic())
+        parent->UpdateMobilityHierarchy(MobilitySettings::STATIC);
+    else if (!(colliderType == ColliderType::STATIC) && parent->IsStatic())
+        parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+
+    userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    App->GetPhysicsModule()->CreateCapsuleRigidBody(this);
+}
+
+CapsuleColliderComponent::~CapsuleColliderComponent()
+{
+    App->GetPhysicsModule()->DeleteCapsuleRigidBody(this);
+}
+
+void CapsuleColliderComponent::Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) const
+{
+    Component::Save(targetState, allocator);
+
+    targetState.AddMember("FreezeRotation", freezeRotation, allocator);
+    targetState.AddMember("Mass", mass, allocator);
+    targetState.AddMember("ColliderType", (int)colliderType, allocator);
+    targetState.AddMember("ColliderLayer", (int)layer, allocator);
+    targetState.AddMember("Radius", radius, allocator);
+    targetState.AddMember("Length", length, allocator);
+    targetState.AddMember("GenerateCallback", generateCallback, allocator);
+    targetState.AddMember("FitToSize", fitToSize, allocator);
+
+    // CENTER OFFSET
+    rapidjson::Value centerOffsetSave(rapidjson::kArrayType);
+    centerOffsetSave.PushBack(centerOffset.x, allocator)
+        .PushBack(centerOffset.y, allocator)
+        .PushBack(centerOffset.z, allocator);
+    targetState.AddMember("CenterOffset", centerOffsetSave, allocator);
+
+    // CENTER ROTATION
+    rapidjson::Value centerRotationSave(rapidjson::kArrayType);
+    centerRotationSave.PushBack(centerRotation.x, allocator)
+        .PushBack(centerRotation.y, allocator)
+        .PushBack(centerRotation.z, allocator);
+    targetState.AddMember("CenterRotation", centerRotationSave, allocator);
+}
+
+void CapsuleColliderComponent::Clone(const Component* other)
+{
+}
+
+void CapsuleColliderComponent::RenderEditorInspector()
+{
+    if (ImGui::BeginCombo("Collider type", ColliderTypeStrings[(int)colliderType]))
+    {
+        int colliderStringSize = sizeof(ColliderTypeStrings) / sizeof(char*);
+        for (int i = 0; i < colliderStringSize; ++i)
+        {
+            if (ImGui::Selectable(ColliderTypeStrings[i]))
+            {
+                colliderType = ColliderType(i);
+                if (colliderType == ColliderType::STATIC)
+                {
+                    parent->UpdateMobilityHierarchy(MobilitySettings::STATIC);
+                    mass = 0.f;
+                }
+                else
+                {
+                    parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+                    mass = 1.f;
+                }
+                App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+            }
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::BeginDisabled(colliderType == ColliderType::STATIC);
+    if (ImGui::InputFloat("Mass", &mass))
+    {
+        App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+    }
+    ImGui::EndDisabled();
+
+    if (ImGui::InputFloat3("Center offset", &centerOffset[0])) App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+
+    if (ImGui::InputFloat("Radius", &radius)) App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+    if (ImGui::InputFloat("Length", &length)) App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+
+    if (ImGui::Checkbox("Freeze rotation", &freezeRotation)) App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+
+    if (ImGui::InputFloat3("Center rotation", &centerRotation[0]))
+        App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+
+    // COLLIDER LAYER SETTINGS
+    if (ImGui::BeginCombo("Layer options", ColliderLayerStrings[(int)layer]))
+    {
+        const int colliderStringSize = sizeof(ColliderLayerStrings) / sizeof(char*);
+        for (int i = 0; i < colliderStringSize; ++i)
+        {
+            if (ImGui::Selectable(ColliderLayerStrings[i]))
+            {
+                layer = ColliderLayer(i);
+                App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+            }
+        }
+        ImGui::EndCombo();
+    }
+
+    if (ImGui::Checkbox("Fit to size", &fitToSize))
+    {
+        CalculateCollider();
+        App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+    }
+
+    if (ImGui::Checkbox("Generate Callbacks", &generateCallback))
+    {
+        userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    }
+}
+
+void CapsuleColliderComponent::Update(float deltaTime)
+{
+}
+
+void CapsuleColliderComponent::Render(float deltaTime)
+{
+}
+
+void CapsuleColliderComponent::ParentUpdated()
+{
+    if (fitToSize) CalculateCollider();
+
+    if (parent->IsStatic() && colliderType != ColliderType::STATIC)
+    {
+        mass = 0.f;
+        colliderType = ColliderType::STATIC;
+    }
+    else if (!parent->IsStatic() && colliderType == ColliderType::STATIC)
+    {
+        mass         = 1.f;
+        colliderType = ColliderType::DYNAMIC;
+    }
+
+    App->GetPhysicsModule()->UpdateCapsuleRigidBody(this);
+}
+
+void CapsuleColliderComponent::OnCollision(GameObject* otherObject, float3 collisionNormal)
+{
+}
+
+void CapsuleColliderComponent::CalculateCollider()
+{
+    AABB heriachyAABB = parent->GetHierarchyAABB();
+
+    if (heriachyAABB.IsFinite() && !heriachyAABB.IsDegenerate())
+    {
+        radius       = heriachyAABB.Size().MaxElement() / 2.f;
+        length       = heriachyAABB.Size().y / 2.f;
+        centerOffset = heriachyAABB.CenterPoint() - parent->GetPosition();
+    }
+}
---
+++ SobrassadaEngine/Scene/Components/Standalone/Physics/CubeColliderComponent.cpp
@@ -0,0 +1,214 @@
+#include "CubeColliderComponent.h"
+
+#include "Application.h"
+#include "GameObject.h"
+#include "PhysicsModule.h"
+
+#include "ImGui.h"
+#include "Libs/rapidjson/document.h"
+
+CubeColliderComponent::CubeColliderComponent(UID uid, GameObject* parent)
+    : Component(uid, parent, "Cube Collider", COMPONENT_CUBE_COLLIDER)
+{
+
+    parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+
+    CalculateCollider();
+
+    onCollissionCallback = CollisionDelegate(
+        std::bind(&CubeColliderComponent::OnCollision, this, std::placeholders::_1, std::placeholders::_2)
+    );
+    userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    App->GetPhysicsModule()->CreateCubeRigidBody(this);
+}
+
+CubeColliderComponent::CubeColliderComponent(const rapidjson::Value& initialState, GameObject* parent)
+    : Component(initialState, parent)
+{
+    if (initialState.HasMember("FreezeRotation")) freezeRotation = initialState["FreezeRotation"].GetBool();
+    if (initialState.HasMember("Mass")) mass = initialState["Mass"].GetFloat();
+    if (initialState.HasMember("ColliderType")) colliderType = ColliderType(initialState["ColliderType"].GetInt());
+    if (initialState.HasMember("ColliderLayer")) layer = ColliderLayer(initialState["ColliderLayer"].GetInt());
+    if (initialState.HasMember("GenerateCallback")) generateCallback = initialState["GenerateCallback"].GetBool();
+    if (initialState.HasMember("FitToSize")) fitToSize = initialState["FitToSize"].GetBool();
+
+    if (initialState.HasMember("CenterOffset"))
+    {
+        const rapidjson::Value& dataArray = initialState["CenterOffset"];
+        centerOffset                      = {dataArray[0].GetFloat(), dataArray[1].GetFloat(), dataArray[2].GetFloat()};
+    }
+
+    if (initialState.HasMember("CenterRotation"))
+    {
+        const rapidjson::Value& dataArray = initialState["CenterRotation"];
+        centerRotation                    = {dataArray[0].GetFloat(), dataArray[1].GetFloat(), dataArray[2].GetFloat()};
+    }
+
+    if (initialState.HasMember("Size"))
+    {
+        const rapidjson::Value& dataArray = initialState["Size"];
+        size                              = {dataArray[0].GetFloat(), dataArray[1].GetFloat(), dataArray[2].GetFloat()};
+    }
+
+    if (colliderType == ColliderType::STATIC && !parent->IsStatic())
+        parent->UpdateMobilityHierarchy(MobilitySettings::STATIC);
+    else if (!(colliderType == ColliderType::STATIC) && parent->IsStatic())
+        parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+
+    onCollissionCallback = CollisionDelegate(
+        std::bind(&CubeColliderComponent::OnCollision, this, std::placeholders::_1, std::placeholders::_2)
+    );
+    userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    App->GetPhysicsModule()->CreateCubeRigidBody(this);
+}
+
+CubeColliderComponent::~CubeColliderComponent()
+{
+    App->GetPhysicsModule()->DeleteCubeRigidBody(this);
+}
+
+void CubeColliderComponent::Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) const
+{
+    Component::Save(targetState, allocator);
+
+    targetState.AddMember("FreezeRotation", freezeRotation, allocator);
+    targetState.AddMember("Mass", mass, allocator);
+    targetState.AddMember("ColliderType", (int)colliderType, allocator);
+    targetState.AddMember("ColliderLayer", (int)layer, allocator);
+    targetState.AddMember("GenerateCallback", generateCallback, allocator);
+    targetState.AddMember("FitToSize", fitToSize, allocator);
+
+    // CENTER OFFSET
+    rapidjson::Value centerOffsetSave(rapidjson::kArrayType);
+    centerOffsetSave.PushBack(centerOffset.x, allocator)
+        .PushBack(centerOffset.y, allocator)
+        .PushBack(centerOffset.z, allocator);
+    targetState.AddMember("CenterOffset", centerOffsetSave, allocator);
+
+    // CENTER ROTATION
+    rapidjson::Value centerRotationSave(rapidjson::kArrayType);
+    centerRotationSave.PushBack(centerRotation.x, allocator)
+        .PushBack(centerRotation.y, allocator)
+        .PushBack(centerRotation.z, allocator);
+    targetState.AddMember("CenterRotation", centerRotationSave, allocator);
+
+    // BOX SIZE
+    rapidjson::Value sizeSave(rapidjson::kArrayType);
+    sizeSave.PushBack(size.x, allocator).PushBack(size.y, allocator).PushBack(size.z, allocator);
+    targetState.AddMember("Size", sizeSave, allocator);
+}
+
+void CubeColliderComponent::Clone(const Component* other)
+{
+}
+
+void CubeColliderComponent::RenderEditorInspector()
+{
+
+    if (ImGui::BeginCombo("Collider type", ColliderTypeStrings[(int)colliderType]))
+    {
+        const int colliderStringSize = sizeof(ColliderTypeStrings) / sizeof(char*);
+        for (int i = 0; i < colliderStringSize; ++i)
+        {
+            if (ImGui::Selectable(ColliderTypeStrings[i]))
+            {
+                colliderType = ColliderType(i);
+                if (colliderType == ColliderType::STATIC)
+                {
+                    parent->UpdateMobilityHierarchy(MobilitySettings::STATIC);
+                    mass = 0.f;
+                }
+
+                else
+                {
+                    parent->UpdateMobilityHierarchy(MobilitySettings::DYNAMIC);
+                    mass = 1.f;
+                }
+
+                App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+            }
+        }
+        ImGui::EndCombo();
+    }
+
+    ImGui::BeginDisabled(colliderType == ColliderType::STATIC);
+    if (ImGui::InputFloat("Mass", &mass))
+    {
+        App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+    }
+    ImGui::EndDisabled();
+
+    if (ImGui::InputFloat3("Center offset", &centerOffset[0])) App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+
+    if (ImGui::InputFloat3("Size", &size[0])) App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+
+    if (ImGui::Checkbox("Freeze rotation", &freezeRotation)) App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+
+    if (ImGui::InputFloat3("Center rotation", &centerRotation[0])) App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+
+    // COLLIDER LAYER SETTINGS
+    if (ImGui::BeginCombo("Layer options", ColliderLayerStrings[(int)layer]))
+    {
+        const int colliderStringSize = sizeof(ColliderLayerStrings) / sizeof(char*);
+        for (int i = 0; i < colliderStringSize; ++i)
+        {
+            if (ImGui::Selectable(ColliderLayerStrings[i]))
+            {
+                layer = ColliderLayer(i);
+                App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+            }
+        }
+        ImGui::EndCombo();
+    }
+
+    if (ImGui::Checkbox("Fit to size", &fitToSize))
+    {
+        CalculateCollider();
+        App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+    }
+
+    if (ImGui::Checkbox("Generate Callbacks", &generateCallback))
+    {
+        userPointer = BulletUserPointer(this, &onCollissionCallback, generateCallback);
+    }
+}
+
+void CubeColliderComponent::Update(float deltaTime)
+{
+}
+
+void CubeColliderComponent::Render(float deltaTime)
+{
+}
+
+void CubeColliderComponent::ParentUpdated()
+{
+    if (fitToSize) CalculateCollider();
+
+    if (parent->IsStatic() && colliderType != ColliderType::STATIC)
+    {
+        mass         = 0.f;
+        colliderType = ColliderType::STATIC;
+    }
+    else if (!parent->IsStatic() && colliderType == ColliderType::STATIC)
+    {
+        mass         = 1.f;
+        colliderType = ColliderType::DYNAMIC;
+    }
+
+    App->GetPhysicsModule()->UpdateCubeRigidBody(this);
+}
+
+void CubeColliderComponent::OnCollision(GameObject* otherObject, float3 collisionNormal)
+{
+}
+
+void CubeColliderComponent::CalculateCollider()
+{
+    AABB heriachyAABB = parent->GetHierarchyAABB();
+    if (heriachyAABB.IsFinite() && !heriachyAABB.IsDegenerate())
+    {
+        size         = heriachyAABB.HalfSize();
+        centerOffset = heriachyAABB.CenterPoint() - parent->GetPosition();
+    }
+}
