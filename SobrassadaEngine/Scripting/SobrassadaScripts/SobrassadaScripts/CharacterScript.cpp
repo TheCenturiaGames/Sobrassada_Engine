7c599760418ed826450de02b16e5bd69886b20b7
Marc López <marclop.se@gmail.com>
2025-04-15 17:01:13 +0200

    Creation & simple implementation of CharacterScript
    Creating the class and prepare it for the basis.

---
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/CharacterScript.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CharacterScript.h"
+#include "Component.h"
+#include "Scene/Components/Standalone/AnimationComponent.h"
+#include "ResourceStateMachine.h"
+#include "GameObject.h"
+
+
+bool CharacterScript::Init()
+{
+    GLOG("Initiating CharacterScript");
+    return true;
+}
+
+void CharacterScript::Update(float deltaTime)
+{
+    AnimationComponent* animComponent = parent
+}
