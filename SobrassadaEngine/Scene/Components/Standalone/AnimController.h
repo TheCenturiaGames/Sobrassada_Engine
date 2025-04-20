0b648624fe64e45f12f2e5e2b521962a8190db9f
Gold <dzharzard96@gmail.com>
2025-04-20 18:00:23 +0200

    Erase scale lerps
    Erased scale lerps because we dont need them and is not advisable in general.

--- SobrassadaEngine/Scene/Components/Standalone/AnimController.h
+++ SobrassadaEngine/Scene/Components/Standalone/AnimController.h
@@ -17,7 +17,7 @@ class AnimController
     void Resume() { playAnimation = true; }
 
     void GetTransform(const std::string& nodeName, float3& pos, Quat& rot);
-    void GetTransform(const std::string& nodeName, float3& pos, Quat& rot, float3& scale);
+
     ResourceAnimation* GetCurrentAnimation() const { return currentAnimation; }
     float GetTime() const { return currentTime; }
 
@@ -30,7 +30,7 @@ class AnimController
   private:
     void GetChannelPosition(const Channel* animChannel, float3& pos, float time) const;
     void GetChannelRotation(Channel* animChannel, Quat& rot, float time);
-    void GetChannelScale(const Channel* animChannel, float3& scale, const float time) const;
+   
     void SetAnimationResource(ResourceAnimation* anim) { currentAnimation = anim; }
 
     Quat Interpolate(Quat& first, Quat& second, float lambda);
