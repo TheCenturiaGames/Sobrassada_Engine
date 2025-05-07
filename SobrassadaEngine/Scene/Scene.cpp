aaeac183469d681e27a1d0f8942805016a1caa05
caspi99 <65339956+caspi99@users.noreply.github.com>
2025-05-07 23:36:30 +0200

    THO-486 add new game object (#160)
    * Solved bug
    
    * Solved
    
    * When creating a gameobject it creates on root node
    
    * Update Scene.cpp

--- SobrassadaEngine/Scene/Scene.cpp
+++ SobrassadaEngine/Scene/Scene.cpp
@@ -560,6 +560,7 @@ void Scene::RenderHierarchyUI(bool& hierarchyMenu)
 
     if (ImGui::Button("Add GameObject"))
     {
+
         GameObject* parent = GetGameObjectByUID(selectedGameObjectUID);
         if (parent != nullptr)
         {
@@ -570,6 +571,31 @@ void Scene::RenderHierarchyUI(bool& hierarchyMenu)
 
             newGameObject->UpdateTransformForGOBranch();
         }
+    }
+
+    if (ImGui::IsWindowHovered() && ImGui::IsMouseClicked(ImGuiMouseButton_Right))
+    {
+        ImGui::OpenPopup("HierarchyContextMenu");
+    }
+
+    if (ImGui::BeginPopup("HierarchyContextMenu"))
+    {
+        if (ImGui::MenuItem("Add GameObject"))
+        {
+            GameObject* parent = GetGameObjectByUID(gameObjectRootUID);
+
+            if (parent != nullptr)
+            {
+                GameObject* newGameObject = new GameObject(gameObjectRootUID, "new Game Object");
+
+                gameObjectsContainer.insert({newGameObject->GetUID(), newGameObject});
+                parent->AddGameObject(newGameObject->GetUID());
+
+                newGameObject->UpdateTransformForGOBranch();
+            }
+        }
+
+        ImGui::EndPopup();
     }
 
     if (selectedGameObjectUID != gameObjectRootUID)
@@ -1006,7 +1032,8 @@ GameObject* Scene::GetGameObjectByUID(UID gameObjectUUID)
 
 GameObject* Scene::GetGameObjectByName(const std::string& name)
 {
-    // TODO: Replace gameObject name to a HashString, I've seen it is also compared in some scripts and would improve performance
+    // TODO: Replace gameObject name to a HashString, I've seen it is also compared in some scripts and would improve
+    // performance
 
     // Returns the first object with that name, if there are more they are ignored
     for (const auto& obj : gameObjectsContainer)
