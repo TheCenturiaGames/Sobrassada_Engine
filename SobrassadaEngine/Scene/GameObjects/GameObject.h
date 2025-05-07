469f29ea312d8b4f5e362fc9882c307e404cc3f4
Bartomeu Perelló <44304824+IDarkgenesis@users.noreply.github.com>
2025-05-07 14:20:41 +0200

    THO-476 Invert mouse picking (#144)
    * Object picking inverted
    
    * Selecting game objects expands the tree view
    
    * Const
    
    * Loading single nodes in gltf should be fixed

--- SobrassadaEngine/Scene/GameObjects/GameObject.h
+++ SobrassadaEngine/Scene/GameObjects/GameObject.h
@@ -61,7 +61,7 @@ class SOBRASADA_API_ENGINE GameObject
     const float4x4& GetParentGlobalTransform() const;
 
     bool IsStatic() const { return mobilitySettings == MobilitySettings::STATIC; };
-    bool IsTopParent() const { return isTopParent; };
+    bool HasSelectParent() const { return selectParent; };
     bool IsNavMeshValid() const { return navMeshValid; };
     bool IsComponentCreated(int i) const { return createdComponents[i]; };
 
@@ -113,6 +113,7 @@ class SOBRASADA_API_ENGINE GameObject
     void UpdateTransformForGOBranch();
     void UpdateMobilityHierarchy(MobilitySettings type);
     void UpdateLocalTransform(const float4x4& parentGlobalTransform);
+    void UpdateOpenNodeHierarchy(bool openValue);
 
     bool WillUpdate() const { return willUpdate; };
 
@@ -143,6 +144,8 @@ class SOBRASADA_API_ENGINE GameObject
     void SetEnabled(bool state) { enabled = state; }
     void SetComponentCreated(int position) { createdComponents[position] = true; }
     void SetComponentRemoved(int position) { createdComponents[position] = false; }
+    void SetSelectParent(bool newSelectParent) { selectParent = newSelectParent; }
+    void SetOpenHierarchyNode(bool newOpen) { openHierarchyNode = newOpen; }
 
   private:
     void DrawNodes() const;
@@ -179,10 +182,11 @@ class SOBRASADA_API_ENGINE GameObject
 
     ComponentType selectedComponentIndex = COMPONENT_NONE;
     int mobilitySettings                 = STATIC;
-    bool isTopParent                     = false;
+    bool selectParent                    = false;
     bool willUpdate                      = false;
     bool enabled                         = true;
     bool navMeshValid                    = false;
+    bool openHierarchyNode               = false;
 
     std::tuple<COMPONENTS> compTuple     = std::make_tuple(COMPONENTS_NULLPTR);
     std::bitset<std::tuple_size<decltype(compTuple)>::value> createdComponents;
